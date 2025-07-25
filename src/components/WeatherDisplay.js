7306c0b31ab828fdc7b0cdd692476954df064381
BharatJivvTate <hibharatj@gmail.com>
2025-07-25 17:30:00 +0530

    fixed error

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,17 +1,17 @@
-import React from 'react'
+    import React from 'react'
 
-const WeatherDisplay = ({weather}) => {
-    console.log(weather)
-    const tempStyle = {
-        color: weather.temperature > 20 ? 'red' : 'blue'
+    const WeatherDisplay = ({weather}) => {
+        console.log(weather)
+        const tempStyle = {
+            color: weather.temperature > 20 ? 'red' : 'blue'
+        }
+
+    return (
+        <div>
+        <p>Temperature: <span style={tempStyle}>{weather.temperature}</span></p>
+        <p>Conditions: {weather.conditions}</p>
+        </div>
+    )
     }
 
-  return (
-    <div>
-      <p>Temperature: <span style={tempStyle}>{weather.temperature}</span>
-      Conditions: <span>{weather.conditions}</span></p>
-    </div>
-  )
-}
-
-export default WeatherDisplay
+    export default WeatherDisplay
